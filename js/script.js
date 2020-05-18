c6ef8f142a55274489e1b13820b1cb4acbffacac
S.R. Scott <srandallscott@gmail.com>
2020-05-18 16:14:52 -0400

    script.js - more display logic

--- js/script.js
+++ js/script.js
@@ -74,6 +74,7 @@ const cardsArray = [
     },
 ];
 
+// ============================================================
 
 // Grab the div with an id of root
 const game = document.getElementById('game');
@@ -84,3 +85,22 @@ grid.setAttribute('class', 'grid');
 
 // Append the grid section to the game div
 game.appendChild(grid);
+
+// ============================================================
+
+cardsArray.forEach(item => {
+    // Create a div
+    const card = document.createElement('div')
+  
+    // Apply a card class to that div
+    card.classList.add('card')
+  
+    // Set the data-name attribute of the div to the cardsArray name
+    card.dataset.name = item.name
+  
+    // Apply the background image of the div to the cardsArray image
+    card.style.backgroundImage = `url(${item.img})`
+  
+    // Append the div to the grid section
+    grid.appendChild(card)
+  })
